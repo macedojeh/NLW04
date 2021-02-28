2bb1c95149e00546b4127d1af8163eb54ee21407
Jéssica Macedo <jessicaamacedo@hotmail.com>
2021-02-28 17:47:48 -0300

    settime fixed

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -36,7 +36,7 @@ export function CountdownProvider({ children } : CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
